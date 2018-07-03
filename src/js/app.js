266c61a07f0e2337034594e0a87f1bbace1a41dc
Krysta Knight <klknight23@gmail.com>
2018-07-03 15:07:57 -0500

    Add iscool filter

--- src/js/app.js
+++ src/js/app.js
@@ -9,9 +9,51 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+//////////////////////////////////////////////////////////////////////
+//5 places to see in St.Louis
+var busch = {
+  location: {
+    lat: 38.6226,
+    lng: -90.1928
+  },
+  name: 'Busch Stadium'
+}
+var zoo = {
+  location: {
+    lat: 38.6367,
+    lng: -90.2932
+  },
+  name: 'St. Louis Zoo'
+}
+
+var cityMuseum = {
+  location: {
+    lat: 38.6336,
+    lng: -90.2006
+  },
+  name: 'St. Louis City Museum'
+}
+
+var arch = {
+  location: {
+    lat: 38.6247,
+    lng: -90.1848
+  },
+  name: 'St. Louis Arch'
+}
+
+var gardens = {
+  location: {
+    lat: 38.6128,
+    lng: -90.2594
+  },
+  name: 'Missouri Botanical Garden'
+}
+
+
 ////////////////////////MAP////////////////////////////////
 var map;
-var completePlaces = [];
+var completePlaces = ko.observableArray();
 var CLIENT_ID = "4Q1OVHHXPENTRL3JQN5ODBJJPUNTO2NGB2APYO5JYDKHGLRK";
 var CLIENT_SECRET = "ZL4NPBEBU1114V2KY5PEHYVQ21VENYDFAQPYWIWISGBPWDNV";
 //Initailize map
@@ -28,75 +70,45 @@ function initMap() {
       center: stl
     });
 
-  //////////////////////////////////////////////////////////////////////
-  // place object creates marker for each place unpon intilization and returns place object
-  var Place = function(name, location, addr, rating) {
-    var content = "<div>" + name + "</div><div>" + addr;
-    var infowindow = new google.maps.InfoWindow({
-      content: content
-    });
-    var marker = new google.maps.Marker({
-      position: location,
-      map: map
-    });
-    marker.addListener("click", function() {
-      infowindow.open(map, marker);
-    });
+    // place object creates marker for each place unpon intilization and returns place object
+    var Place = function(name, location, addr, icon) {
+      var content = "<div>" + name + "</div><div>" + addr+"</div>";
+      var infowindow = new google.maps.InfoWindow({
+        content: content
+      });
+      var marker = new google.maps.Marker({
+        position: location,
+        map: map
+      });
+      marker.addListener("click", function() {
+        infowindow.open(map, marker);
+      });
 
-    var place = {
-      name: name, //pre-set
-      location: location, // pre-set
-      addr: addr, //from foursquare api
-      rating: rating, //from foursqaure api
-      marker: marker,
-      infowindow: infowindow,
-      show: true //default to true
-    };
+      //chooses a random number to base filter on
+      function isCool(){
+        var num = Math.floor((Math.random() * 5) + 1);
+        if(num > 3){
+          return true;
+        }else{
+          return false;
+        }
+      }
 
-    return place;
-  }
+      var place = {
+        name: name, //pre-set
+        location: location, // pre-set
+        addr: addr, //from foursquare api
+        icon: icon, //from foursqaure api
+        marker: marker,
+        isCool: isCool(),
+        infowindow: infowindow,
+        show: ko.observable(true) //default to true
+      };
 
-  //5 places to see in St.Louis
-  var busch = {
-    location: {
-      lat: 38.6226,
-      lng: -90.1928
-    },
-    name: 'Busch Stadium'
-  }
-  var zoo = {
-    location: {
-      lat: 38.6367,
-      lng: -90.2932
-    },
-    name: 'St. Louis Zoo'
-  }
+      return place;
+    }
 
-  var cityMuseum = {
-    location: {
-      lat: 38.6336,
-      lng: -90.2006
-    },
-    name: 'St. Louis City Museum'
-  }
-
-  var arch = {
-    location: {
-      lat: 38.6247,
-      lng: -90.1848
-    },
-    name: 'St. Louis Arch'
-  }
-
-  var gardens = {
-    location: {
-      lat: 38.6128,
-      lng: -90.2594
-    },
-    name: 'Botanical Gardens'
-  }
-
-  //call the foursqaure api for each place and get address and rating, then create a new Place object
+  //call the foursqaure api for each place and get address and icon, then create a new Place object
   var simplePlaces = [busch, zoo, arch, cityMuseum, gardens];
   simplePlaces.forEach(getInfo)
 
@@ -116,13 +128,17 @@ function initMap() {
         var result = data.response.venues[0];
 
         var location = result.hasOwnProperty('location') ? result.location : '';
-        if (location.hasOwnProperty('address')) {
-          item.address = location.address || '';
+        if (location.hasOwnProperty('formattedAddress')) {
+          var address = location.formattedAddress || "Not Available";
         }
-        var rating = result.hasOwnProperty('rating') ? result.rating : '';
-        item.rating = rating || 'none';
 
-        var newPlace = Place(item.name, item.location, item.address, item.rating)
+        var category = result.hasOwnProperty('categories') ? result.categories : '';
+        var getCategory = category[0]
+        var prefix = getCategory.icon.prefix
+        var suffix = getCategory.icon.suffix
+        var icon = prefix + "88" + suffix
+
+        var newPlace = Place(item.name, item.location, address, icon)
         completePlaces.push(newPlace)
 
       },
@@ -133,16 +149,27 @@ function initMap() {
   }
 }
 
-var ViewModel = function() {
 
-  this.placeList = completePlaces;
+var ViewModel = function() {
+  var cpLength = completePlaces().length;
 
   this.showAll = function() {
-
+    for(var i = 0; i < cpLength; i++){
+      if(!completePlaces()[i].isCool){
+        completePlaces()[i].show(true)
+      }
+    }
   };
 
-  this.showFree = function() {
-
+  this.showCool = function() {
+    console.log("showcool")
+    for(var i = 0; i < cpLength; i++){
+      if(!completePlaces()[i].isCool){
+        completePlaces()[i].show(false)
+        console.log("Uncool Places")
+        console.log(completePlaces()[i])
+      }
+    }
   };
 
 };
@@ -150,9 +177,3 @@ var ViewModel = function() {
 //array.push is async so must wait to create ViewModel
 const wait = time => new Promise((resolve) => setTimeout(resolve, time));
 wait(3000).then(() => ko.applyBindings(new ViewModel()));
-
-
-
-
-
-// 5 places to see in St. Louis
